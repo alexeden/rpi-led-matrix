0266ce91a03e20a8ec7d486f2e5ba7820bd8d865
Alex Eden <alexandereden91@gmail.com>
2019-03-22 22:57:29 -0500

    Separate NodeLedMatrix declaration into header file

---
+++ src/node-led-matrix.h
@@ -0,0 +1,15 @@
+#ifndef NODELEDMATRIX_H
+#define NODELEDMATRIX_H
+
+#include <napi.h>
+#include <led-matrix.h>
+
+class NodeLedMatrix : public Napi::ObjectWrap<NodeLedMatrix> {
+public:
+	static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    NodeLedMatrix(const Napi::CallbackInfo &info);
+private:
+	static Napi::FunctionReference constructor;
+};
+
+#endif
