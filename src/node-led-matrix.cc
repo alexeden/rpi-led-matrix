2d7a4cbf3f9dabf424472cdf5870cffea0b76173
Alex Eden <alexandereden91@gmail.com>
2019-03-23 00:12:28 -0500

    Start moving the option generate methods to static methods of the class

--- src/node-led-matrix.cc
+++ src/node-led-matrix.cc
@@ -1,5 +1,7 @@
 #include "node-led-matrix.h"
 
+using namespace rgb_matrix;
+
 Napi::FunctionReference NodeLedMatrix::constructor;
 
 Napi::Object NodeLedMatrix::Init(Napi::Env env, Napi::Object exports) {
@@ -24,4 +26,32 @@ Napi::Object NodeLedMatrix::Init(Napi::Env env, Napi::Object exports) {
 }
 
 NodeLedMatrix::NodeLedMatrix(const Napi::CallbackInfo &info) : Napi::ObjectWrap<NodeLedMatrix>(info) {
+}
+
+
+/**
+ * Generate and return the default matrix options as a JS object.
+ */
+Napi::Object NodeLedMatrix::defaultMatrixOptions(const Napi::CallbackInfo& info) {
+	auto env = info.Env();
+
+	RGBMatrix::Options options = RGBMatrix::Options();
+
+	auto obj = Napi::Object::New(env);
+	obj.Set("brightness", Napi::Number::New(env, options.brightness));
+	obj.Set("chain_length", Napi::Number::New(env, options.chain_length));
+	obj.Set("cols", Napi::Number::New(env, options.cols));
+	obj.Set("multiplexing", Napi::Number::New(env, options.multiplexing));
+	obj.Set("parallel", Napi::Number::New(env, options.parallel));
+	obj.Set("pwm_bits", Napi::Number::New(env, options.pwm_bits));
+	obj.Set("pwm_dither_bits", Napi::Number::New(env, options.pwm_dither_bits));
+	obj.Set("pwm_lsb_nanoseconds", Napi::Number::New(env, options.pwm_lsb_nanoseconds));
+	obj.Set("row_address_type", Napi::Number::New(env, options.row_address_type));
+	obj.Set("rows", Napi::Number::New(env, options.rows));
+	obj.Set("scan_mode", Napi::Number::New(env, options.scan_mode));
+	obj.Set("disable_hardware_pulsing", Napi::Boolean::New(env, options.disable_hardware_pulsing));
+	obj.Set("inverse_colors", Napi::Boolean::New(env, options.inverse_colors));
+	obj.Set("show_refresh_rate", Napi::Boolean::New(env, options.show_refresh_rate));
+
+	return obj;
 }
--- src/node-led-matrix.h
+++ src/node-led-matrix.h
@@ -9,8 +9,15 @@ class NodeLedMatrix : public Napi::ObjectWrap<NodeLedMatrix> {
 public:
 	static Napi::Object Init(Napi::Env env, Napi::Object exports);
     NodeLedMatrix(const Napi::CallbackInfo &info);
+
+	static Napi::Object defaultMatrixOptions(const Napi::CallbackInfo& info);
+	static Napi::Object defaultRuntimeOptions(const Napi::CallbackInfo& info);
+
 private:
 	static Napi::FunctionReference constructor;
+	static rgb_matrix::RGBMatrix::Options createMatrixOptions(const Napi::CallbackInfo& info);
+	static rgb_matrix::RuntimeOptions createRuntimeOptions(const Napi::CallbackInfo& info);
+
 };
 
 #endif
