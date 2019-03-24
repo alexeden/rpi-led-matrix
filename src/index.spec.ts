55611395ee5ca576dba1b844a8b98eb3e579f1ca
Alex Eden <alexandereden91@gmail.com>
2019-03-23 23:40:26 -0500

    Clean up the spec runner

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,60 +1,71 @@
 import { addon } from './addon';
 import { MatrixOptions, RuntimeOptions, GpioMapping } from './types';
 
-try {
-  console.log('addon: ', addon);
-  console.log('addon.LedMatrix: ', addon.LedMatrix);
-  console.log('addon.LedMatrix.defaultMatrixOptions(): ', addon.LedMatrix.defaultMatrixOptions());
-  console.log('addon.LedMatrix.defaultRuntimeOptions(): ', addon.LedMatrix.defaultRuntimeOptions());
-  const matrixOpts: MatrixOptions = {
-    ...addon.LedMatrix.defaultMatrixOptions(),
-    rows: 32,
-    cols: 64,
-    chain_length: 2,
-    // disable_hardware_pulsing: true,
-    hardware_mapping: GpioMapping.AdafruitHatPwm,
-  };
-  const runtimeOpts: RuntimeOptions = {
-    ...addon.LedMatrix.defaultRuntimeOptions(),
-    gpio_slowdown: 0,
-  };
+const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
 
-  const instance = new addon.LedMatrix(matrixOpts, runtimeOpts);
-  console.log('new addon.LedMatrix: ', instance);
-  // tslint:disable-next-line:no-any
-  console.log('instance.pwmBits(): ', instance.pwmBits());
-  console.log('instance.pwmBits(1): ', instance.pwmBits(1));
-  console.log('instance.pwmBits(12): ', instance.pwmBits(12));
-  console.log('instance.pwmBits(11): ', instance.pwmBits(11));
-  console.log('instance.brightness(): ', instance.brightness());
-  console.log('instance.brightness(0): ', instance.brightness(0));
-  console.log('instance.brightness(100): ', instance.brightness(100));
-  console.log('instance.height(): ', instance.height());
-  console.log('instance.width(): ', instance.width());
+(async () => {
+  try {
+    console.log('addon: ', addon);
+    console.log('addon.LedMatrix: ', addon.LedMatrix);
+    console.log('addon.LedMatrix.defaultMatrixOptions(): ', addon.LedMatrix.defaultMatrixOptions());
+    console.log('addon.LedMatrix.defaultRuntimeOptions(): ', addon.LedMatrix.defaultRuntimeOptions());
+    const matrixOpts: MatrixOptions = {
+      ...addon.LedMatrix.defaultMatrixOptions(),
+      rows: 32,
+      cols: 64,
+      chain_length: 2,
+      hardware_mapping: GpioMapping.AdafruitHatPwm,
+    };
+    const runtimeOpts: RuntimeOptions = {
+      ...addon.LedMatrix.defaultRuntimeOptions(),
+      gpio_slowdown: 0,
+    };
+
+    const instance = new addon.LedMatrix(matrixOpts, runtimeOpts);
+    console.log('new addon.LedMatrix: ', instance);
+    console.log('instance.pwmBits(): ', instance.pwmBits());
+    console.log('instance.pwmBits(1): ', instance.pwmBits(1));
+    console.log('instance.pwmBits(12): ', instance.pwmBits(12));
+    console.log('instance.pwmBits(11): ', instance.pwmBits(11));
+    console.log('instance.brightness(): ', instance.brightness());
+    console.log('instance.brightness(0): ', instance.brightness(0));
+    console.log('instance.brightness(100): ', instance.brightness(100));
+    console.log('instance.height(): ', instance.height());
+    console.log('instance.width(): ', instance.width());
 
 
-  const interval = 333;
-  setTimeout(() => instance.fill(0, 0, 255), 0);
-  setTimeout(() => instance.fill(0, 255, 0), 1 * interval);
-  setTimeout(() => instance.fill(255, 0, 0), 2 * interval);
-  setTimeout(() => instance.clear(), 3 * interval);
+    const interval = 333;
+    instance.fill(0, 0, 255);
+    await wait(interval);
+    instance.fill(0, 255, 0);
+    await wait(interval);
+    instance.fill(255, 0, 0);
+    await wait(interval);
+    instance.clear();
+    await wait(interval);
 
-  setTimeout(
-    () => {
-      const y = Math.floor(instance.height() / 2);
-      Array.from(Array(instance.width())).map((_, x) => {
-        instance.setPixel(x, y, 255, 0, 0);
-      });
-    },
-    4 * interval
-  );
+    const y = Math.floor(instance.height() / 2);
+    Array.from(Array(instance.width())).map((_, x) => {
+      instance.setPixel(x, y, 255, 0, 0);
+    });
+    await wait(interval);
 
-  setTimeout(() => console.log('bye!'), 5000);
+    console.log('instance.luminanceCorrect(true): ', instance.luminanceCorrect(true));
+    // console.log('instance.brightness(50): ', instance.brightness(50));
+    instance.fill(0, 255, 0);
+    await wait(2000);
+    console.log('instance.luminanceCorrect(false): ', instance.luminanceCorrect(false));
+    instance.fill(0, 255, 0);
+    await wait(2000);
+    // console.log('instance.pwmBits(1): ', instance.pwmBits(1));
 
-}
-catch (error) {
-  console.error(error);
-}
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+})();
+
 
 /*
 
