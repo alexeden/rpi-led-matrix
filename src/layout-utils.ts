5d7fb7375097ad7e3cedf9606a4a56995c16e7af
Alex Eden <alexandereden91@gmail.com>
2019-03-30 22:50:09 -0500

    Start rewriting the text layout engine

--- src/layout-utils.ts
+++ src/layout-utils.ts
@@ -1,12 +1,19 @@
 import { FontInstance } from './types';
 
-interface Glyph {
+export interface Glyph {
   h: number;
   w: number;
   char: string;
 }
 
-type MappedGlyph = Glyph & { x: number; y: number };
+export interface MappedGlyph extends Glyph {
+  x: number;
+  y: number;
+}
+
+export type Word = Glyph[];
+export type Line = Word[];
+
 
 class TextWrapper {
   readonly lines: Glyph[][] = [[]];
@@ -80,12 +87,89 @@ interface WrappedText {
   fits: boolean;
 }
 
+const isSeparator = ({ char }: Glyph) => char === ' ';
+
+export const glphysToWords = (glphys: Glyph[]): Word[] => {
+  const index = glphys
+    .map((g, i) => i === 0 && isSeparator(g) ? null : g.char)
+    .indexOf(' ');
+
+  return index > 0
+    ? [glphys.slice(0, index), ...glphysToWords(glphys.slice(index))]
+    : [glphys];
+};
+
+export const calcWordWidth = (gs: Glyph[]) => gs.reduce((sum, { w }) => sum + w, 0);
+
+export const wordsToLines = (maxWidth: number, words: Word[]): Line[] => {
+  const lines: Line[] = [];
+  let tmpLine: Line = [];
+  let tmpLineWidth = 0;
+
+  words.filter(({ length }) => length > 0).forEach(word => {
+    const wordWidth = calcWordWidth(word);
+    if (tmpLineWidth + wordWidth > maxWidth) {
+      lines.push(tmpLine);
+      const firstWord = word.filter(g => !isSeparator(g));
+      tmpLine = [ firstWord ];
+      tmpLineWidth = calcWordWidth(firstWord);
+    }
+    else {
+      tmpLine.push(word);
+      tmpLineWidth += wordWidth;
+    }
+  });
+
+  if (tmpLine.length > 0) lines.push(tmpLine);
+
+  return lines;
+};
+
+export enum HorizontalAlignment {
+  Left = 'left',
+  Center = 'center',
+  Right = 'right',
+}
+
+export enum VerticalAlignment {
+  Bottom = 'bottom',
+  Middle = 'middle',
+  Top = 'top',
+}
+
 export class LayoutUtils {
+  static wrapTextToLines(font: FontInstance, maxW: number, text: string): Line[] {
+    const fontHeight = font.height();
+    const glphys = text.split('').map(char => ({
+      char,
+      h: fontHeight,
+      w: font.stringWidth(char),
+    }));
+
+    return wordsToLines(maxW, glphysToWords(glphys));
+  }
+
+  static mapLinesToContainer(lines: Line[], lineH: number, containerW: number, containerH: number): MappedGlyph[] {
+
+    return [];
+  }
+
   static wrapText(font: FontInstance, containerW: number, h: number, text: string): WrappedText {
     const wrapper = new TextWrapper(font, containerW, h);
+    const fontHeight = font.height();
 
-    const words = text.split(' ');
-    words.forEach(word => {
+    const gs: Glyph[] = text.split('').map(char => ({
+      char,
+      h: fontHeight,
+      w: font.stringWidth(char),
+    }));
+
+    const words = glphysToWords(gs);
+    const lines = wordsToLines(containerW, words);
+
+    console.log(JSON.stringify(lines, null, 4));
+
+    text.split(' ').forEach(word => {
       wrapper.addWord(word);
     });
 
