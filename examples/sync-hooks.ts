0d130754b782021923ffa02cca2b70b9aec0e6cc
Alex Eden <alexandereden91@gmail.com>
2019-06-17 18:59:52 -0500

    Tweak sync hook example

--- examples/sync-hooks.ts
+++ examples/sync-hooks.ts
@@ -6,8 +6,8 @@ import { matrixOptions, runtimeOptions } from './_config';
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
 
-    matrix.afterSync((m, dt, t) => {
-      console.log(`After sync hook called! Time since last sync = ${dt}`);
+    matrix.afterSync((mat, dt, t) => {
+      console.log(`After sync hook called! Time since last sync = ${dt}, current time ${t}`);
     });
 
     setInterval(() => matrix.sync(), 1000);
