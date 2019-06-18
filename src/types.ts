8b435a123827d9dae4335adfebd4185f0c4c3e61
Alex Eden <alexandereden91@gmail.com>
2019-06-18 16:07:33 -0500

    Allow drawBuffer to accept a UInt8Array

--- src/types.ts
+++ src/types.ts
@@ -232,7 +232,7 @@ export interface LedMatrixInstance {
   clear(): this;
   clear(x0: number, y0: number, x1: number, y1: number): this;
 
-  drawBuffer(buffer: Buffer, w?: number, h?: number): this;
+  drawBuffer(buffer: Buffer | Uint8Array, w?: number, h?: number): this;
   drawCircle(x: number, y: number, r: number): this;
   drawLine(x0: number, y0: number, x1: number, y1: number): this;
   drawRect(x0: number, y0: number, width: number, height: number): this;
