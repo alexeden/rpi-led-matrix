7b24f830436787b4162dfa78bac10b67c452ef33
Alex Eden <alexandereden91@gmail.com>
2019-10-18 23:07:12 -0500

    Implement an example of map

--- examples/map.ts
+++ examples/map.ts
@@ -1,36 +1,20 @@
 import { LedMatrix } from '../src';
 import { matrixOptions, runtimeOptions } from './_config';
 
-class Pulser {
-  constructor(
-    readonly x: number,
-    readonly y: number,
-    readonly f: number
-  ) { }
+const nextColor = (f: number, t: number): number => {
+  const brightness = 0xFF & Math.max(0, 255 * Math.sin(f * t / 1000));
 
-  nextColor(t: number): number {
-    /** You could easily work position-dependent logic into this expression */
-    const brightness = 0xFF & Math.max(0, (128 * (Math.sin(this.f * t / 1000))));
-
-    return (brightness << 16) | (brightness << 8) | brightness;
-  }
-}
+  return (brightness << 16) | (brightness << 8) | brightness;
+};
 
 (async () => {
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
-
-    const pulsers: Pulser[] = [];
-
-    for (let x = 0; x < matrix.width(); x++) {
-      for (let y = 0; y < matrix.height(); y++) {
-        pulsers.push(new Pulser(x, y, 10 * Math.random()));
-      }
-    }
+    const freqs = [...Array(matrix.width() * matrix.height()).keys()].map(i => i / 20);
 
     matrix.afterSync((mat, dt, t) => {
-      pulsers.forEach(pulser => {
-        matrix.fgColor(pulser.nextColor(t)).setPixel(pulser.x, pulser.y);
+      matrix.map(([x, y, i]) => {
+        return nextColor(freqs[i], t);
       });
 
       setTimeout(() => matrix.sync(), 0);
