f2487d98090e51d289e6a58e276757186a6b959e
Alex Eden <alexandereden91@gmail.com>
2019-11-11 20:08:10 -0600

    Scaffold a chainlink pixel mapper

---
+++ src/chain-link-pixel-mapper.cc
@@ -0,0 +1,22 @@
+#include <pixel-mapper.h>
+
+class ChainLinkPixelMapper : public rgb_matrix::PixelMapper {
+  public:
+	ChainLinkPixelMapper() {
+	}
+
+	virtual const char *GetName() const {
+		return "ChainLink";
+	}
+
+    virtual bool SetParameters(int chain, int parallel, const char *param) {
+        return true;
+    }
+
+    virtual bool GetSizeMapping(int matrix_width, int matrix_height, int *visible_width, int *visible_height) const {
+        return true;
+    }
+
+    virtual void MapVisibleToMatrix(int matrix_width, int matrix_height, int x, int y, int *matrix_x, int *matrix_y) const {
+    }
+};
