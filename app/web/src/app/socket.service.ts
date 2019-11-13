f49bb2eae5ade2c532ad6efd5b8fa4a33cf27dc5
Alex Eden <alexandereden91@gmail.com>
2019-11-12 22:40:11 -0600

    Setup the socket service

--- app/web/src/app/socket.service.ts
+++ app/web/src/app/socket.service.ts
@@ -1,16 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
+import { switchMap, retryWhen, takeUntil, publishReplay } from 'rxjs/operators';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
+type Message = { };
 @Injectable({
   providedIn: 'root',
 })
 export class SocketService {
+  private readonly url$ = new Subject<string>();
+  private readonly stopSocket$ = new Subject<any>();
+  private readonly retrySocket$ = new Subject<any>();
+  readonly socketError = new Subject<Event>();
+  readonly connected$ = new BehaviorSubject<boolean>(false);
+  readonly message: Observable<Message>;
 
-  constructor() { }
+  socket: WebSocketSubject<Message> | null = null;
+
+  constructor() {
+    this.message = this.url$.pipe(
+      switchMap(url => {
+        const socket = webSocket<Message>(url);
+        this.socket = socket;
+
+        return socket.multiplex(
+          /* Open   */ () => this.connected$.next(true),
+          /* Close  */ () => this.connected$.next(false),
+          /* Filter */ () => true
+        )
+        .pipe(
+          retryWhen(error => {
+            error.subscribe(this.socketError);
+
+            return this.retrySocket$;
+          }),
+          takeUntil(this.stopSocket$)
+        );
+      }),
+      publishReplay(1)
+    );
+
+    (this.message as any).connect();
+
+  }
 
 
   connect() {
+    if (!this.connected$.getValue()) {
+      this.url$.next(`wss://${location.host}/api/socket`);
+    }
+  }
+
+  retryConnect() {
+    this.retrySocket$.next('retrying');
   }
 
   disconnect() {
+    this.stopSocket$.next('disconnect!');
   }
 }
