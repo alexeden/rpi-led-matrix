5ccb522e5da012d7b0dd29a027b4148a7e8e6f24
Alex Eden <alexandereden91@gmail.com>
2019-11-12 22:35:00 -0600

    Scaffold a socket service

--- app/web/src/app/app.module.ts
+++ app/web/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocketService } from './socket.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    SocketService,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
