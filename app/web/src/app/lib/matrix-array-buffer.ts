989ebca8596b8c540d43308f614bd3f932592ea7
Alex Eden <alexandereden91@gmail.com>
2019-11-13 20:16:14 -0600

    Nix the row marker idea for now

--- app/web/src/app/lib/matrix-array-buffer.ts
+++ app/web/src/app/lib/matrix-array-buffer.ts
@@ -1,12 +1,24 @@
 export class MatrixArrayBuffer extends ArrayBuffer {
   static readonly pixelBytes = 3;
-  static readonly rowIdBytes = 3;
+  // static readonly rowIdBytes = 4;
+
+  private readonly colInds: number[];
+  private readonly rowInds: number[];
 
   constructor(
-    readonly rows: number,
-    readonly cols: number
+    readonly m: number,
+    readonly n: number
   ) {
-    super(MatrixArrayBuffer.pixelBytes * rows * (cols + MatrixArrayBuffer.rowIdBytes));
+    super(MatrixArrayBuffer.pixelBytes * m * n);
     (window as any).matrixArrayBuffer = this;
+
+    this.rowInds = [...Array(m).keys()];
+    this.colInds = [...Array(n).keys()];
+
+    // // Set the row marker bytes
+    // const bytesPerRow = n + MatrixArrayBuffer.rowIdBytes;
+    // this.rowInds.forEach(r => {
+    //   const
+    // });
   }
 }
