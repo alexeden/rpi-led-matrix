596bb60f8b48ae5c577ec7a65907cdb58862d576
Alex Eden <alexandereden91@gmail.com>
2019-11-13 20:39:23 -0600

    Create an abstraction for a Pixel

--- app/web/src/app/lib/matrix-array-buffer.ts
+++ app/web/src/app/lib/matrix-array-buffer.ts
@@ -1,3 +1,30 @@
+class Pixel {
+  constructor(
+    readonly row: number,
+    readonly col: number,
+    readonly data: DataView
+  ) {
+    switch (this.col % 3) {
+      case 0: this.hex(0xFF0000); break;
+      case 1: this.hex(0x00FF00); break;
+      case 2: this.hex(0x0000FF); break;
+    }
+  }
+
+  hex(hex?: number) {
+    if (typeof hex === 'number') {
+      this.data.setUint8(0, hex & 0xFF);
+      this.data.setUint8(1, hex >> 8 & 0xFF);
+      this.data.setUint8(2, hex >> 16 & 0xFF);
+    }
+    else {
+      return (this.data.getUint8(0) << 16)
+        | (this.data.getUint8(1) << 8)
+        | (this.data.getUint8(2));
+    }
+  }
+}
+
 export class MatrixArrayBuffer extends ArrayBuffer {
   static readonly pixelBytes = 3;
   // static readonly rowIdBytes = 4;
@@ -5,20 +32,27 @@ export class MatrixArrayBuffer extends ArrayBuffer {
   private readonly colInds: number[];
   private readonly rowInds: number[];
 
+  private readonly pixels: Pixel[];
+
   constructor(
-    readonly m: number,
-    readonly n: number
+    readonly rows: number,
+    readonly cols: number
   ) {
-    super(MatrixArrayBuffer.pixelBytes * m * n);
+    super(MatrixArrayBuffer.pixelBytes * rows * cols);
     (window as any).matrixArrayBuffer = this;
 
-    this.rowInds = [...Array(m).keys()];
-    this.colInds = [...Array(n).keys()];
+    this.rowInds = [...Array(rows).keys()];
+    this.colInds = [...Array(cols).keys()];
 
     // // Set the row marker bytes
     // const bytesPerRow = n + MatrixArrayBuffer.rowIdBytes;
     // this.rowInds.forEach(r => {
     //   const
     // });
+    this.pixels = this.rowInds.flatMap(r =>
+      this.colInds.map(c =>
+        new Pixel(r, c, new DataView(this, 3 * (r * cols + c), 3))
+      )
+    );
   }
 }
