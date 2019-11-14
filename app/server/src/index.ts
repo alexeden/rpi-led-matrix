fa509b6232031708141dcf4105cd95a89b4c3528
Alex Eden <alexandereden91@gmail.com>
2019-11-13 23:09:23 -0600

    Start building the matrix on the server

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -5,8 +5,33 @@ import * as fs from 'fs';
 import * as express from 'express';
 import * as net from 'net';
 import * as websockets from 'ws';
+import {
+  GpioMapping,
+  LedMatrix,
+  LedMatrixUtils,
+  MatrixOptions,
+  PixelMapperType,
+  RuntimeOptions,
+} from 'rpi-led-matrix';
 
 const matrixConfig = require('../../matrix.config.json');
+
+const matrixOptions: MatrixOptions = {
+  ...LedMatrix.defaultMatrixOptions(),
+  rows: 32,
+  cols: 64,
+  chainLength: 2,
+  hardwareMapping: GpioMapping.Regular,
+  parallel: 3,
+  pixelMapperConfig: LedMatrixUtils.encodeMappers(
+    { type: PixelMapperType.Chainlink }
+  ),
+};
+
+export const runtimeOptions: RuntimeOptions = {
+  ...LedMatrix.defaultRuntimeOptions(),
+  gpioSlowdown: 1,
+};
 
 const httpsOptions: https.ServerOptions = {
   key: fs.readFileSync(path.resolve(__dirname, '..', 'server.key')),
@@ -16,18 +41,15 @@ const httpsOptions: https.ServerOptions = {
 const app = express();
 const server = https.createServer(httpsOptions, app).listen(4000, '0.0.0.0', () => console.log('listening on port 4000'));
 const wss = new websockets.Server({ noServer: true });
+const liveSockets = new Set<websockets>([]);
 
 app.get('/api/config', (req, res, next) => {
   res.json(matrixConfig);
 });
 
-
 server.on('upgrade', (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
   wss.handleUpgrade(request, socket, head, clientSocket => wss.emit('connection', clientSocket, request));
 });
-
-const liveSockets = new Set<websockets>([]);
-
 
 wss.on('connection', (socket, req) => {
   console.log('new socket connection');
