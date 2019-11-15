e3a6fa3d3f5a630ff41970bef7411124f0a013ed
Alex Eden <alexandereden91@gmail.com>
2019-11-15 11:14:56 -0600

    Add an isSupported flag to package exports

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ export * from './types';
 export * from './utils';
 
 import { LedMatrixAddon } from './types';
-const { Font, LedMatrix }: LedMatrixAddon = require('bindings')('rpi-led-matrix');
+const { Font, isSupported, LedMatrix }: LedMatrixAddon = require('bindings')('rpi-led-matrix');
 
 export {
   Font,
+  isSupported,
   LedMatrix,
 };
