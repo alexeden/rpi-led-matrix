66a51df6895fdf19e03408949cabd30b7f1c13b0
Alex Eden <alexandereden91@gmail.com>
2019-11-14 23:28:39 -0600

    Map the pointer to a circle on the matrix

--- app/web/src/app/canvas/canvas.component.ts
+++ app/web/src/app/canvas/canvas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ElementRef, Renderer2, Input, OnDestroy } from '@ang
 import { CanvasSpace, Pt, Group, CanvasForm, AnimateCallbackFn, Bound, Num } from 'pts';
 import { MatrixConfig, BufferService } from '../buffer.service';
 import { Subject, BehaviorSubject, Observable } from 'rxjs';
-import { share, filter, switchMapTo, takeUntil, tap } from 'rxjs/operators';
+import { share, filter, switchMapTo, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
 import { SocketService } from '../socket.service';
 
 @Component({
@@ -13,11 +13,14 @@ import { SocketService } from '../socket.service';
 export class CanvasComponent implements OnInit, OnDestroy {
   private readonly unsubscribe$ = new Subject<any>();
   private readonly ready$ = new BehaviorSubject(false);
+  // private readonly pointer$ = new BehaviorSubject(new Pt(0, 0));
+  // private readonly canvasRect$ = new BehaviorSubject(new ClientRect());
   private readonly ctx: CanvasRenderingContext2D;
 
   readonly form: CanvasForm;
   readonly space: CanvasSpace;
   readonly animate: Observable<{ t: number; dt: number }>;
+  readonly pointer: Observable<Pt>;
 
   constructor(
     private readonly elRef: ElementRef,
@@ -33,7 +36,7 @@ export class CanvasComponent implements OnInit, OnDestroy {
     this.space = new CanvasSpace(this.canvas, () => this.ready$.next(true)).setup({
       bgcolor: '0xFF0000',
       resize: false,
-      retina: true,
+      retina: false,
     });
 
     this.form = new CanvasForm(this.space);
@@ -41,6 +44,21 @@ export class CanvasComponent implements OnInit, OnDestroy {
     this.animate = new Observable(subscriber => {
       this.space.add((t, dt) => subscriber.next({ t: t!, dt: dt! }));
     });
+
+    this.pointer = new Observable(subscriber => {
+      let { left, top } = this.canvas.getBoundingClientRect();
+      this.space.add({
+        resize: () => {
+          const rect = this.canvas.getBoundingClientRect();
+          left = rect.left;
+          top = rect.top;
+          // { left, top } = rect;
+          // { left, top } = this.canvas.getBoundingClientRect();
+        },
+        action: (t, x, y) => subscriber.next(new Pt(x - left, y - top)),
+      });
+    });
+
   }
 
 
@@ -59,12 +77,18 @@ export class CanvasComponent implements OnInit, OnDestroy {
       takeUntil(this.unsubscribe$)
     )
     .subscribe(config => {
+      // console.log('lets play!');
     });
 
-    this.space.add((time, ftime) => {
-      const radius = Num.cycle((time! % 1000) / 1000) * 100;
-      this.form.fill('#09f').point(this.space.pointer, radius, 'circle');
-    });
+    this.animate.pipe(
+      takeUntil(this.unsubscribe$),
+      withLatestFrom(this.pointer, ({ t }, ptr) => {
+        const radius = Num.cycle((t % 1000) / 1000) * 10;
+        this.form.fill('#09f').point(ptr, radius, 'circle');
+      })
+    )
+    .subscribe();
+
     this.space.bindMouse().play();
   }
 
