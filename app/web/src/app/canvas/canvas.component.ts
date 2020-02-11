01344c07e747a334f538e4def18ff5882f77630a
Alex Eden <alexandereden91@gmail.com>
2020-02-11 00:58:13 -0600

    Putting this on hold for a bit

--- app/web/src/app/canvas/canvas.component.ts
+++ app/web/src/app/canvas/canvas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, Renderer2, Input, OnDestroy } from '@angular/core';
-import { CanvasSpace, Pt, Group, CanvasForm, AnimateCallbackFn, Bound, Num, Color } from 'pts';
+import { CanvasSpace, Pt, Group, CanvasForm, AnimateCallbackFn, Bound, Num, Color, World, Create, Particle } from 'pts';
 import { MatrixConfig, BufferService } from '../buffer.service';
 import { Subject, BehaviorSubject, Observable } from 'rxjs';
 import { share, filter, switchMapTo, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
@@ -63,6 +63,8 @@ export class CanvasComponent implements OnInit, OnDestroy {
 
 
   ngOnInit() {
+    const world = new World(this.space.innerBound, 1, 100);
+
     this.ready$.pipe(
       filter(ready => ready),
       switchMapTo(this.bufferService.config),
@@ -82,18 +84,52 @@ export class CanvasComponent implements OnInit, OnDestroy {
 
     this.animate.pipe(
       takeUntil(this.unsubscribe$),
-      withLatestFrom(this.pointer, ({ t }, ptr) => {
+      withLatestFrom(this.pointer, ({ t, dt }, ptr) => {
         const radius = Math.round(Num.cycle((t % 1000) / 1000) * 10);
         const [x, y] = ptr.map(Math.round);
-        const color = Color.hsl(x % 360, 1, 0.5);
+        // const color = Color.hsl(x % 360, 1, 0.5);
         this.form
-          .fill(Color.HSLtoRGB(color).toString('rgb'))
-          .point(ptr, radius, 'circle')
+          // .fill(Color.HSLtoRGB(color).toString('rgb'))
+          // .point(ptr, radius, 'circle')
           .textBox([new Pt(x, y).add(radius + 5, -20), new Pt(x, y).add(100, 20)], `${x + radius}, ${y}`, 'middle');
+
+        world.particle(0).position = new Pt(x, y);
+        world.drawParticles((p, i) => {
+          const color = (i === 0) ? '#fff' : ['#ff2d5d', '#42dc8e', '#2e43eb', '#ffe359'][i % 4];
+          this.form.fillOnly(color).point(p, p.radius, 'circle');
+        });
+
+        world.update(dt!);
+
       })
     )
     .subscribe();
 
+
+
+    this.space.add({
+      start: () => {
+        const pts = Create.distributeRandom(this.space.innerBound, 100);
+
+        // Create particles and hit them with a random impulse
+        for (let i = 0, len = pts.length; i < len; i++) {
+          const p = new Particle(pts[i]).size((i === 0)
+            ? 1
+            : 1 + Math.random() * this.space.size.x / 50);
+          p.hit(Num.randomRange(-50, 50), Num.randomRange(-25, 25));
+          world.add(p);
+        }
+
+        // world.particle(0).lock = true; // lock it to move it by pointer later on
+      },
+      animate: (time, ftime) => {
+      },
+      action: (type, px, py) => {
+      },
+      resize: (bound, evt) => {
+        if (world) world.bound = this.space.innerBound;
+      }
+    });
     this.space.bindMouse().play();
   }
 
