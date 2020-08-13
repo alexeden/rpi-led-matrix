462bbacbc9ba637425189eec60fc0bee823f1247
Alex Eden <alexandereden91@gmail.com>
2020-08-13 00:04:52 -0500

    Verify drawBuffer works at a fast enough FPS

--- examples/draw-buffer.ts
+++ examples/draw-buffer.ts
@@ -1,20 +1,24 @@
 import { LedMatrix } from '../src';
 import { matrixOptions, runtimeOptions } from './_config';
 
-const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
-
 (async () => {
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
 
     matrix.clear().brightness(100);
 
-    const buffer = Buffer.of(
-      ...[...Array(matrix.width() * matrix.height() * 3).keys()].map(() => Math.random() > 0.4 ? 0xFF : 0x00)
-    );
+    const baseBuffer = [...Array(matrix.width() * matrix.height() * 3).keys()];
+    const buffer1 = Buffer.of(...baseBuffer.map(() => Math.random() < 0.1 ? 0xFF : 0x00));
+    const buffer2 = Buffer.of(...baseBuffer.map(() => Math.random() < 0.1 ? 0xFF : 0x00));
 
-    matrix.drawBuffer(buffer).sync();
-    await wait(999999999);
+    let useBuffer1 = true;
+    matrix.afterSync(() => {
+      useBuffer1 = !useBuffer1;
+      matrix.drawBuffer(useBuffer1 ? buffer1 : buffer2);
+      setTimeout(() => matrix.sync(), 17);
+    });
+
+    matrix.sync();
   }
   catch (error) {
     console.error(`${__filename} caught: `, error);
