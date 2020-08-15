0865ed9e8e6a5ad19967397aa35b27ce7524be01
Alex Eden <alexandereden91@gmail.com>
2020-08-14 20:31:57 -0500

    Yeah nevermind on that

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,22 @@
-import { PixelMapper, PixelMapperType, } from './types';
+import { PixelMapper, PixelMapperType, } from "./types";
 
 export class LedMatrixUtils {
   static encodeMappers(...mappers: PixelMapper[]): string {
     return mappers
       .map(mapper => {
         switch (mapper.type) {
-          case PixelMapperType.Chainlink: return PixelMapperType.Chainlink;
-          case PixelMapperType.Rotate: return [ PixelMapperType.Rotate, mapper.angle, ].join(':');
-          case PixelMapperType.U: return PixelMapperType.U;
-          case PixelMapperType.V: return PixelMapperType.V;
-          case PixelMapperType.VZ: return PixelMapperType.VZ;
+          case PixelMapperType.Chainlink:
+            return PixelMapperType.Chainlink;
+          case PixelMapperType.Rotate:
+            return [ PixelMapperType.Rotate, mapper.angle, ].join(":");
+          case PixelMapperType.U:
+            return PixelMapperType.U;
+          case PixelMapperType.V:
+            return PixelMapperType.V;
+          case PixelMapperType.VZ:
+            return PixelMapperType.VZ;
         }
       })
-      .join(';');
+      .join(";");
   }
 }
