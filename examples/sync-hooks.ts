ab9ac0ec36ffd481eee8602f614dbfb43b77d4e8
Alex Eden <alexandereden91@gmail.com>
2025-02-08 20:27:30 -0600

    Update the sync hooks example

--- examples/sync-hooks.ts
+++ examples/sync-hooks.ts
@@ -1,21 +1,26 @@
 import { LedMatrix } from '../src';
+import * as color from 'color';
 import { matrixOptions, runtimeOptions } from './_config';
+
+const rainbow64 = Array.from(Array(384))
+  .map((_, i, { length }) => Math.floor((360 * i) / length))
+  .map(hue => color.hsl(hue, 100, 50).rgb().array());
 
 class Pulser {
   constructor(readonly x: number, readonly y: number, readonly f: number) {}
 
   nextColor(t: number): number {
-    /** You could easily work position-dependent logic into this expression */
-    const brightness = 0xff & Math.max(0, 255 * Math.sin((this.f * t) / 1000));
-
-    return (brightness << 16) | (brightness << 8) | brightness;
+    const brightness = Math.max(0, Math.sin((this.f * t) / 1000));
+    const [r, g, b] = rainbow64[Math.round(this.x + this.y + t / 100) % 384];
+    return (
+      ((brightness * r) << 16) | ((brightness * g) << 8) | (brightness * b)
+    );
   }
 }
 
 (() => {
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
-
     const pulsers: Pulser[] = [];
 
     for (let x = 0; x < matrix.width(); x++) {
