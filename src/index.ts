3d2f7f8a878806113c493b7ed00a4ac56af65969
Alex Eden <alexandereden91@gmail.com>
2025-02-12 17:45:46 -0600

    Use createRequire to import the binary

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import * as bindings from '../build/Release/rpi-led-matrix.node';
+import { createRequire } from 'node:module';
 import { type LedMatrixAddon } from './types';
 export * from './layout-utils';
 export * from './types';
 export * from './utils';
-export { Font, isSupported, LedMatrix };
-const { Font, isSupported, LedMatrix } = bindings as LedMatrixAddon;
+const require = createRequire(import.meta.url);
+export const { Font, isSupported, LedMatrix } =
+  require('../build/Release/rpi-led-matrix.node') as LedMatrixAddon;
+// import * as bindings from '../build/Release/rpi-led-matrix.node';
+// export { Font, isSupported, LedMatrix };
+//  bindings as LedMatrixAddon;
