857ba6770bd4c854872d147c7e20fc3fb7f7e2e3
Alex Eden <alexandereden91@gmail.com>
2025-02-12 18:08:34 -0600

    pita

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { createRequire } from 'node:module';
 import { type LedMatrixAddon } from './types';
 export * from './layout-utils';
 export * from './types';
 export * from './utils';
+
+const require = globalThis.require ?? createRequire(import.meta.url);
+// createRequire(import.meta.url);
+
 export const { Font, isSupported, LedMatrix } =
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   require('../build/Release/rpi-led-matrix.node') as LedMatrixAddon;
