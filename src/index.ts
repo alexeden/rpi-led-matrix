947cfab5397e39eb794a60654ac15aba5212eb0e
Alex Eden <alexandereden91@gmail.com>
2025-02-12 18:01:46 -0600

    nvrmd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import { createRequire } from 'node:module';
 import { type LedMatrixAddon } from './types';
 export * from './layout-utils';
 export * from './types';
 export * from './utils';
-const require = createRequire(import.meta.url);
 export const { Font, isSupported, LedMatrix } =
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   require('../build/Release/rpi-led-matrix.node') as LedMatrixAddon;
-// import * as bindings from '../build/Release/rpi-led-matrix.node';
-// export { Font, isSupported, LedMatrix };
-//  bindings as LedMatrixAddon;
